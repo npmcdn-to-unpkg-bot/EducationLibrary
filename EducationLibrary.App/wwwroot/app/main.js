4204ed745cedcf9fca455aa5a78c3b18289cd946
Alexey Stupakov <alexey.stupakov@gmail.com>
2016-08-16 09:45:23 +0300

    Some changes

--- EducationLibrary.App/wwwroot/app/main.js
+++ EducationLibrary.App/wwwroot/app/main.js
@@ -1,9 +1,5 @@
 /// <reference path="../../typings/globals/core-js/index.d.ts" />
 "use strict";
-//import { bootstrap }    from '@angular/platform-browser-dynamic';
-//import { AppComponent } from './app.component';
-//import { HTTP_PROVIDERS } from '@angular/http';
-//bootstrap(AppComponent, [HTTP_PROVIDERS]);
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var app_module_1 = require('./app.module');
 platform_browser_dynamic_1.platformBrowserDynamic().bootstrapModule(app_module_1.AppModule);
