7bbbe1dfa2bbfa4ce757bbb1f6fae8c3413678e1
Alexey Stupakov <alexey.stupakov@gmail.com>
2016-08-16 12:01:19 +0300

    Delete library DBConnection

--- EducationLibrary.App/wwwroot/_references.js
+++ EducationLibrary.App/wwwroot/_references.js
@@ -8,7 +8,4 @@
 /// <reference path="app/model.js" />
 /// <reference path="app/test.service.js" />
 /// <reference path="js/site.js" />
-/// <reference path="lib/jquery/dist/jquery.js" />
-/// <reference path="lib/jquery-validation/dist/jquery.validate.js" />
-/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
 /// <reference path="systemjs.config.js" />
